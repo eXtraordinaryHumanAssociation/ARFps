4c6202d6e26daec2e3b6f089a6be33766cc5f03a
maguslin <lchen36@gmail.com>
2016-10-29 16:39:09 +0800

    amapfps

---
+++ Assets/Scripts/Utility/AMapUtility/AMapController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class AMapController : MonoBehaviour
+{
+	public Text txtLocation;
+	public Text txtInfo;
+	public AMapListener amap;
+	public AndroidJavaClass jcu;
+	public AndroidJavaObject jou;
+	public AndroidJavaObject mLocationClient;
+	public AndroidJavaObject mLocationOption;
+
+}
+
